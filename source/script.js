2a2f8ddbb13e73790253061d8bb0fc03eda63a7a
zibarnumaldamax <zibarnumaldamax@gmail.com>
2023-06-23 13:37:56 +0300

    corrections

--- source/script.js
+++ source/script.js
@@ -1,6 +1,7 @@
 const meat = document.getElementById("meat-counter");
 const rice = document.getElementById("rice-counter");
 const croissant = document.getElementById("croissant-counter");
+var played = true;
 
 function animateValue(obj, start, end, duration) {
     let startTimestamp = null;
@@ -17,11 +18,11 @@ window.requestAnimationFrame(step);
 
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
-      if (entry.isIntersecting && !localStorage.getItem('didRunIntro')) {
+      if (entry.isIntersecting && played) {
         animateValue(meat, 0, 322, 3000);
         animateValue(rice, 0, 228, 2500);
         animateValue(croissant, 0, 69, 2000);
-        localStorage.setItem('didRunIntro', true);
+        played = false;
       }
     });
   });
